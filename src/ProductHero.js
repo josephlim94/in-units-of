e3ef0f9a740fdd7cad08f5765f76dba23f59faee
Joseph Lim <josephlim_94@hotmail.co.uk>
2022-08-14 14:49:24 +0800

    create basic structure

--- src/ProductHero.js
+++ src/ProductHero.js
@@ -1,22 +1,45 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Button';
 import ProductHeroLayout from './ProductHeroLayout';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
 
 const backgroundImage =
   'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
 
+const normalizing_unit = "meter";
+const conversion_map = {
+  "meter": 1,
+  "Ariana Grande": 2
+}
+
 export default function ProductHero() {
-  const [name, setName] = React.useState('Cat in the Hat');
-  const [whatItem, setWhatItem] = React.useState('');
-  const handleChange = (event) => {
-    setName(event.target.value);
+  const conversion_map_keys = Object.keys(conversion_map);
+  const [objectOfInterest, setObjectOfInterest] = React.useState('');
+  const [fromItem, setFromItem] = React.useState(conversion_map_keys[0]);
+  const [toItem, setToItem] = React.useState(conversion_map_keys[0]);
+
+  const calculateResultValue = () => {
+    return 1;
+  }
+
+  const [resultValue, setResultValue] = React.useState(calculateResultValue());
+
+  const handleChangeObjectOfInterest = (event) => {
+    setObjectOfInterest(event.target.value);
+    // Do calculation
+    // Display result
+    setResultValue(calculateResultValue());
   };
-  const handleChangeWhatItem = (event) => {
-    setWhatItem(String(event.target.value));
+
+  const handleChangeFromItem = (event) => {
+    setFromItem(String(event.target.value));
+  };
+
+  const handleChangeToItem = (event) => {
+    setToItem(String(event.target.value));
   };
 
   return (
@@ -33,71 +56,65 @@ export default function ProductHero() {
         src={backgroundImage}
         alt="increase priority"
       />
-      <TextField
-        id="outlined-name"
-        label="Name"
-        variant="outlined"
-        // InputProps={{
-        //   sx: {
-        //     color: "white"
-        //   }
-        // }}
-        // sx={{
-        //   "& .MuiInputLabel-root": { color: 'white' }, //styles the label
-        //   "& .MuiOutlinedInput-root": {
-        //     "& > fieldset": { borderColor: "white" },
-        //   },
-        //   "& .MuiOutlinedInput-root.Mui-focused": {
-        //     "& > fieldset": {
-        //       borderColor: "white"
-        //     }
-        //   },
-        //   "& .MuiOutlinedInput-root:hover": {
-        //     "& > fieldset": {
-        //       borderColor: "white"
-        //     }
-        //   }
-        // }}
-        value={name}
-        onChange={handleChange}
-      />
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={whatItem}
-        onChange={handleChangeWhatItem}
-        sx={{
-          width: 250
-        }}
-      >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
-      </Select>
-      <Typography color="inherit" align="center" variant="h2" marked="center">
+      <Box>
+        <TextField
+          id="outlined-name"
+          // label="Name"
+          variant="outlined"
+          value={objectOfInterest}
+          onChange={handleChangeObjectOfInterest}
+        />
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={fromItem}
+          onChange={handleChangeFromItem}
+          sx={{
+            width: 250
+          }}
+        >
+          {conversion_map_keys.map((conversion_map_key) => (
+            <MenuItem
+              key={conversion_map_key}
+              value={conversion_map_key}
+            >
+              {conversion_map_key}
+            </MenuItem>
+          ))}
+        </Select>
+      </Box>
+
+      <Typography color="inherit" align="center" variant="h1" marked="center">
         in units of
       </Typography>
-      <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
-        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
-      >
-        Enjoy secret offers up to -70% off the best luxury hotels every Sunday.
-      </Typography>
-      <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        component="a"
-        href="/premium-themes/onepirate/sign-up/"
-        sx={{ minWidth: 200 }}
-      >
-        Register
-      </Button>
-      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
-        Discover the experience
-      </Typography>
+
+      <Box>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={toItem}
+          onChange={handleChangeToItem}
+          sx={{
+            width: 250
+          }}
+        >
+          {conversion_map_keys.map((conversion_map_key) => (
+            <MenuItem
+              key={conversion_map_key}
+              value={conversion_map_key}
+            >
+              {conversion_map_key}
+            </MenuItem>
+          ))}
+        </Select>
+        <TextField
+          id="outlined-name"
+          label="is"
+          variant="outlined"
+          disabled
+          value={resultValue}
+        />
+      </Box>
     </ProductHeroLayout>
   );
 }
